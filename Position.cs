ff1443d9f910e72b02ed258307f15443d45ff624
rajaawais32 <rajaawais32@hotmail.com>
2022-10-05 20:10:50 +0400

    Add project files.

---
+++ Position.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MarsRobotMovement
+{
+    public enum Directions
+    {
+        North = 1,//North
+        South = 2,//South
+        East = 3,//East
+        West = 4//West
+    }
+
+    public interface IPosition
+    {
+        void StartMoving(List<int> maxPoints, string moves);
+    }
+
+    public class Position : IPosition
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Directions Direction { get; set; }
+
+        public Position()
+        {
+            X = Y = 0;
+            Direction = Directions.North;
+        }
+
+        private void Rotate90Left()
+        {
+            switch (this.Direction)
+            {
+                case Directions.North:
+                    this.Direction = Directions.West;
+                    break;
+                case Directions.South:
+                    this.Direction = Directions.East;
+                    break;
+                case Directions.East:
+                    this.Direction = Directions.North;
+                    break;
+                case Directions.West:
+                    this.Direction = Directions.South;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void Rotate90Right()
+        {
+            switch (this.Direction)
+            {
+                case Directions.North:
+                    this.Direction = Directions.East;
+                    break;
+                case Directions.South:
+                    this.Direction = Directions.West;
+                    break;
+                case Directions.East:
+                    this.Direction = Directions.South;
+                    break;
+                case Directions.West:
+                    this.Direction = Directions.North;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void MoveInSameDirection()
+        {
+            switch (this.Direction)
+            {
+                case Directions.North:
+                    this.Y += 1;
+                    break;
+                case Directions.South:
+                    this.Y -= 1;
+                    break;
+                case Directions.East:
+                    this.X += 1;
+                    break;
+                case Directions.West:
+                    this.X -= 1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void StartMoving(List<int> maxPoints, string moves)
+        {
+            foreach (var move in moves)
+            {
+                switch (move)
+                {
+                    case 'F':
+                    case 'f':
+                        this.MoveInSameDirection();
+                        break;
+                    case 'L':
+                    case 'l':
+                        this.Rotate90Left();
+                        break;
+                    case 'R':
+                    case 'r':
+                        this.Rotate90Right();
+                        break;
+                    default:
+                        Console.WriteLine($"Invalid Character {move}");
+                        Console.WriteLine($"Suggestion : Only used these 3 Charters [ F , L , R ] ");
+                        break;
+                }
+
+                if (this.X < 0 || this.X > maxPoints[0] || this.Y < 0 || this.Y > maxPoints[1])
+                {
+                    throw new Exception($"Oops! Position can not be beyond bounderies (0 , 0) and ({maxPoints[0]} , {maxPoints[1]})");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using MarsRobotMovement;
+using System;
+using System.Linq;
+
+namespace MarsRoverProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        Restart:
+            try
+            {
+                Console.WriteLine("Please Provide the Grid Size for Rebort Movement, such as 5*5, 3*4, etc");
+                var maxPoints = Console.ReadLine().Trim().Split('*').Select(int.Parse).ToList();
+                if (maxPoints.Count == 2)
+                {
+                    var startPositions = new string[3];
+                    Position position = new Position();
+
+                    if (startPositions.Count() == 3)
+                    {
+                        position.X = 1;
+                        position.Y = 1;
+                        position.Direction = (Directions)Enum.Parse(typeof(Directions), Directions.North.ToString().ToString());
+                    }
+
+                    Console.WriteLine("Please Provide the a string containing multiple commands as described below (Sample command string: LFLRFLFF)");
+                    Console.WriteLine("L --> Turn the robot left");
+                    Console.WriteLine("R --> Turn the robot right");
+                    Console.WriteLine("F --> Move forward on it's facing direction");
+
+                    var moves = Console.ReadLine().ToUpper();
+
+                    try
+                    {
+                        position.StartMoving(maxPoints, moves);
+                        Console.WriteLine(position.X + " , " + position.Y + " , " + position.Direction.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Grid Size");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("If you want to try again then press 'Y' else 'N'");
+            var input = Console.ReadLine();
+
+            if (input == "y" || input == "Y")
+                goto Restart;
+        }
+    }
+}
